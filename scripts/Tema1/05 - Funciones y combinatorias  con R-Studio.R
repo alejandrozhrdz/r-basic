69f3f1e5cd0aa9d69edaa99bf87439bfedb8632c
alejandrozhrdz <45491229+alejandrozhrdz@users.noreply.github.com>
2020-10-05 22:45:22 -0500

    Create 05 - Funciones y combinatorias  con R-Studio.R

---
+++ scripts/Tema1/05 - Funciones y combinatorias  con R-Studio.R
@@ -0,0 +1,35 @@
+#Raiz cuadrada
+sqrt(9)
+#Número de eule exponencial
+exp(2)
+#Valor Absoluto
+abs(-350)
+#Logritmo
+log(9,3)
+log(100,10)
+#Logaritmo base 10
+log10(100)
+
+
+#Número factorial (solo funciona con negativos)
+#El factorial de 0 es 1
+factorial(3)
+factorial(7)
+
+# Coeficiente Binomial: es el número cominatorio o el coeficiente binomial que
+# define  la forma de ordenar n elementos tomados de n en m 
+# n elementos de los cuales se quieren tomar m , obtener m subjconjuntos de 
+# un total de n
+# choose(n,m)
+# Ejemplo: Si tengo 5 platos y me quisiera comer 3 de ellos, de que formas po-
+# dría hacerlo
+
+choose(5,3)
+#El logarimto puede ser visto como una operación opuesta a el exponencial
+log(4^6,4)
+
+6^log(4^6,4)
+
+
+#El triangulo de pascal se forma a partir de los números binomiales apilados uno
+#atrás de otro s
