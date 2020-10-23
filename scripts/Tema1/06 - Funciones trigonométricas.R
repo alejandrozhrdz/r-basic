18c7846cf228a63bf7eb736ef81447c41497d9f6
alejandrozhrdz <alejandrozhrdz@gmail.com>
2020-10-22 19:42:03 -0500

    Minamal fix

---
+++ scripts/Tema1/06 - Funciones trigonométricas.R
@@ -0,0 +1,18 @@
+
+cos(60*pi/180)
+
+sin(60*pi/180)
+
+sinpi(1/2) # = sin(pi/2
+
+cospi(1/2)
+
+tan(pi)   # -1.224647e-16 ~ 0
+
+tan(pi/2) # 1.633124e+16 ~ Inf
+
+asin(0.8660254) #arc sin en radianes
+
+asin(0.8660254) * 180/pi  #arc sin en grados
+
+asin (5) # NaNs arc sin x (-1,1) (corchetes)
